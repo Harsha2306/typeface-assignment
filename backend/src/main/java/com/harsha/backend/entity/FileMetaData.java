660ae98f02075d4c86195e610f6f497dcf7e8d2d
harsharevanthmanchikanti <hr15171517hr@gmail.com>
2025-05-07 11:08:29 +0530

    added index to originalName

--- backend/src/main/java/com/harsha/backend/entity/FileMetaData.java
+++ backend/src/main/java/com/harsha/backend/entity/FileMetaData.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -16,7 +17,7 @@ import java.time.LocalDateTime;
 @Builder
 public class FileMetaData {
   @Id private String id;
-  private String originalName;
+  @Indexed private String originalName;
   private String storedName;
   private String path;
   private String fileType;
